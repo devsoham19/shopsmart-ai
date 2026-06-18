7f9242144d4f41c0e0493c738077a197f6a0e96e
devsoham19 <devsoham19@gmail.com>
2026-06-18 21:50:13 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Welcome to ShopSmart AI</h1>
+      <p>AI Powered Shopping Assistant</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">ShopSmart AI</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
