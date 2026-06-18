372edfae1d5ca032e5205d599fd685e484f8005a
devsoham19 <devsoham19@gmail.com>
2026-06-19 00:04:58 +0530

    Add useState to SearchBar

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./SearchBar.css";
 
 function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="search-container">
       <input
         type="text"
         placeholder="Search products..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
 
       <button>Search</button>
+
+      <p>You typed: {searchTerm}</p>
     </div>
   );
 }
