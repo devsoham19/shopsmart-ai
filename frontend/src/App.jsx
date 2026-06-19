73553aed50d9b77b418f18f06e23178d8564e500
devsoham19 <devsoham19@gmail.com>
2026-06-19 07:32:58 +0530

    Move product data to separate file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,9 @@ import Navbar from "./components/Navbar/Navbar";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ProductCard from "./components/ProductCard/ProductCard";
 
-function App() {
-  const products = [
-    {
-      id: 1,
-      name: "Gaming Laptop",
-      price: 55000,
-    },
-    {
-      id: 2,
-      name: "iPhone",
-      price: 60000,
-    },
-    {
-      id: 3,
-      name: "Wireless Headphones",
-      price: 2500,
-    },
-  ];
+import products from "./Data/Product";
 
+function App() {
   return (
     <>
       <Navbar />
---
+++ frontend/src/Data/Product.js
@@ -0,0 +1,19 @@
+const Products = [
+  {
+    id: 1,
+    name: "Gaming Laptop",
+    price: 55000,
+  },
+  {
+    id: 2,
+    name: "iPhone",
+    price: 60000,
+  },
+  {
+    id: 3,
+    name: "Wireless Headphones",
+    price: 2500,
+  },
+];
+
+export default Products;
